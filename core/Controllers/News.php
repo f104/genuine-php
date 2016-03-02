675c2b14a6f927ccb4c44d0c0e5e99df3f725987
f104 <kirusanov@gmail.com>
2016-03-02 18:21:59 +0400

    Проверка на существование новостей в базе. Иначе был бесконечный редирект.

--- core/Controllers/News.php
+++ core/Controllers/News.php
@@ -5,126 +5,122 @@ namespace Brevis\Controllers;
 use Brevis\Controller as Controller;
 
 class News extends Controller {
-	public $name = 'news';
-	/** @var \Brevis\Model\News $item */
-	public $item = null;
-	public $limit = 2;
-	public $page = 1;
-	private $_offset = 0;
-	private $_total = 0;
 
-	/**
-	 * @param array $params
-	 *
-	 * @return bool
-	 */
-	public function initialize(array $params = array()) {
-		if (empty($params)) {
-			$this->redirect("/{$this->name}/");
-		}
-		elseif ($params[0] === '0') {
-			$this->redirect("/{$this->name}/");
-		}
-		elseif (!empty($params[0])) {
-			if (is_numeric($params[0]) && $params[0] > 1) {
-				if (!isset($params[1]) || !empty($params[1])) {
-					$this->redirect("/{$this->name}/$params[0]/");
-				}
-				$this->page = (int)$params[0];
-				$this->_offset = ($this->page - 1) * $this->limit;
-			}
-			else {
-				$c = $this->core->xpdo->newQuery('Brevis\Model\News', array('alias' => $params[0]));
-				if ($news = $this->core->xpdo->getObject('Brevis\Model\News', $c)) {
-					$this->item = $news;
-				}
-			}
+  public $name = 'news';
 
-			if (!$this->_offset && !$this->item && !$this->isAjax) {
-				$this->redirect("/{$this->name}/");
-			}
-		}
+  /** @var \Brevis\Model\News $item */
+  public $item = null;
+  public $limit = 2;
+  public $page = 1;
+  private $_offset = 0;
+  private $_total = 0;
 
-		return true;
-	}
+  /**
+   * @param array $params
+   *
+   * @return bool
+   */
+  public function initialize(array $params = array()) {
+    if (empty($params)) {
+      $this->redirect("/{$this->name}/");
+    } elseif ($params[0] === '0') {
+      $this->redirect("/{$this->name}/");
+    } elseif (!empty($params[0])) {
+      if (is_numeric($params[0]) && $params[0] > 1) {
+        if (!isset($params[1]) || !empty($params[1])) {
+          $this->redirect("/{$this->name}/$params[0]/");
+        }
+        $this->page = (int) $params[0];
+        $this->_offset = ($this->page - 1) * $this->limit;
+      } else {
+        $c = $this->core->xpdo->newQuery('Brevis\Model\News', array('alias' => $params[0]));
+        if ($news = $this->core->xpdo->getObject('Brevis\Model\News', $c)) {
+          $this->item = $news;
+        }
+      }
 
+      if (!$this->_offset && !$this->item && !$this->isAjax) {
+        $this->redirect("/{$this->name}/");
+      }
+    }
 
-	/**
-	 * @return string
-	 */
-	public function run() {
-		if ($this->isAjax) {
-			if ($this->item) {
-				$this->core->ajaxResponse(false, 'Контроллер News не принимает ajax в режиме показа отдельной новости');
-			}
+    return true;
+  }
 
-			$items = $this->getItems();
-			$pagination = $this->getPagination($this->_total, $this->page, $this->limit);
-			$this->core->ajaxResponse(true, '', array(
-				'items' => $this->template('_news', array('items' => $items), $this),
-				'pagination' => $this->template('_pagination', array('pagination' => $pagination), $this),
-				'total' => $this->_total,
-				'page' => $this->page,
-				'limit' => $this->limit,
-			));
-		}
+  /**
+   * @return string
+   */
+  public function run() {
+    if ($this->isAjax) {
+      if ($this->item) {
+        $this->core->ajaxResponse(false, 'Контроллер News не принимает ajax в режиме показа отдельной новости');
+      }
 
-		if ($this->item) {
-			$data = array(
-				'title' => $this->item->get('pagetitle'),
-				'pagetitle' => $this->item->get('pagetitle'),
-				'longtitle' => $this->item->get('longtitle'),
-				'content' => $this->core->getParser()->text($this->item->get('text')),
-			);
-		}
-		else {
-			$data = array(
-				'title' => 'Новости',
-				'pagetitle' => 'Новости',
-				'items' => $this->getItems(),
-				'pagination' => $this->getPagination($this->_total, $this->page, $this->limit),
-				'content' => '',
-			);
-		}
+      $items = $this->getItems();
+      $pagination = $this->getPagination($this->_total, $this->page, $this->limit);
+      $this->core->ajaxResponse(true, '', array(
+        'items' => $this->template('_news', array('items' => $items), $this),
+        'pagination' => $this->template('_pagination', array('pagination' => $pagination), $this),
+        'total' => $this->_total,
+        'page' => $this->page,
+        'limit' => $this->limit,
+      ));
+    }
 
-		return $this->template('news', $data, $this);
-	}
+    if ($this->item) {
+      $data = array(
+        'title' => $this->item->get('pagetitle'),
+        'pagetitle' => $this->item->get('pagetitle'),
+        'longtitle' => $this->item->get('longtitle'),
+        'content' => $this->core->getParser()->text($this->item->get('text')),
+      );
+    } else {
+      $data = array(
+        'title' => 'Новости',
+        'pagetitle' => 'Новости',
+        'items' => $this->getItems(),
+        'pagination' => $this->getPagination($this->_total, $this->page, $this->limit),
+        'content' => '',
+      );
+    }
 
+    return $this->template('news', $data, $this);
+  }
 
-	/**
-	 * Выбор последних новостей с обрезкой текста
-	 *
-	 * @return array
-	 */
-	public function getItems() {
-		$rows = array();
-		$c = $this->core->xpdo->newQuery('Brevis\Model\News');
-		$this->_total = $this->core->xpdo->getCount('Brevis\Model\News');
-		if ($this->_offset >= $this->_total) {
-			$this->redirect("/{$this->name}/");
-		}
-		$c->select($this->core->xpdo->getSelectColumns('Brevis\Model\News', 'News'));
-		$c->sortby('id', 'DESC');
-		$c->limit($this->limit, $this->_offset);
-		if ($c->prepare() && $c->stmt->execute()) {
-			while ($row = $c->stmt->fetch(\PDO::FETCH_ASSOC)) {
-				$cut = strpos($row['text'], "\n");
-				if ($cut !== false) {
-					$row['text'] = substr($row['text'], 0, $cut);
-					$row['cut'] = true;
-				}
-				else {
-					$row['cut'] = false;
-				}
-				$row['text'] = $this->core->getParser()->text($row['text']);
-				$rows[] = $row;
-			}
-		}
-		else {
-			$this->core->log('Не могу выбрать новости:' . print_r($c->stmt->errorInfo(), true));
-		}
+  /**
+   * Выбор последних новостей с обрезкой текста
+   *
+   * @return array
+   */
+  public function getItems() {
+    $rows = array();
+    $c = $this->core->xpdo->newQuery('Brevis\Model\News');
+    $this->_total = $this->core->xpdo->getCount('Brevis\Model\News');
+    if ($this->_total != 0) {
+      if ($this->_offset >= $this->_total) {
+        $this->redirect("/{$this->name}/");
+      }
+      $c->select($this->core->xpdo->getSelectColumns('Brevis\Model\News', 'News'));
+      $c->sortby('id', 'DESC');
+      $c->limit($this->limit, $this->_offset);
+      if ($c->prepare() && $c->stmt->execute()) {
+        while ($row = $c->stmt->fetch(\PDO::FETCH_ASSOC)) {
+          $cut = strpos($row['text'], "\n");
+          if ($cut !== false) {
+            $row['text'] = substr($row['text'], 0, $cut);
+            $row['cut'] = true;
+          } else {
+            $row['cut'] = false;
+          }
+          $row['text'] = $this->core->getParser()->text($row['text']);
+          $rows[] = $row;
+        }
+      } else {
+        $this->core->log('Не могу выбрать новости:' . print_r($c->stmt->errorInfo(), true));
+      }
 
-		return $rows;
-	}
+      return $rows;
+    }
+  }
 
 }
